ece0d06397ad6e5e39073227d0c83c6227d002ce
radnavr <radnavr@gmail.com>
2024-05-07 17:31:06 +0200

    caching setting @controllers

--- controllers/getOneDeed.js
+++ controllers/getOneDeed.js
@@ -1,6 +1,8 @@
 export const getOneDeed = async () => {
   if (!process.env.NEXT_PUBLIC_API_DOMAIN) return { deed: "placeholder text" };
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/deed`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/deed`, {
+    cache: "no-store",
+  });
   const responseJson = await response.json();
 
   const getRandom = () => Math.floor(Math.random() * responseJson.length);

8c1b278babf3cb2f8c6e228a56e297297e894d15
radnavr <radnavr@gmail.com>
2024-05-07 16:30:32 +0200

    new env names

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,7 +1,10 @@
-export const url = `https://api.cloudinary.com/v1_1/${process.env.CLOUD_NAME}/resources/image/tags/acutis`;
+export const url = `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image/tags/acutis`;
 
 export const headers = new Headers();
 headers.set(
   "Authorization",
-  "Basic " + btoa(`${process.env.API_KEY}:${process.env.API_SECRET}`)
+  "Basic " +
+    btoa(
+      `${process.env.CLOUDINARY_API_KEY}:${process.env.CLOUDINARY_API_SECRET}`
+    )
 );
--- controllers/getOneDeed.js
+++ controllers/getOneDeed.js
@@ -1,5 +1,5 @@
 export const getOneDeed = async () => {
-  const response = await fetch(`${process.env.APP_URL}/api/deed`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/deed`);
   const responseJson = await response.json();
 
   const getRandom = () => Math.floor(Math.random() * responseJson.length);
